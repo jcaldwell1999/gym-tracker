2559ce942a8904061d0f23f53c67cc01e883a0ec
James Caldwell <jamespcaldwell@hotmail.co.uk>
2026-05-08 14:44:12 +0100

    Add top set calculation utility

--- src/utils/progressCalculations.js
+++ src/utils/progressCalculations.js
@@ -1,5 +1,24 @@
+// Calculate total volume of lift
 export function calculateTotalVolume(sets) {
     return sets.reduce((totalVolume, currentSet) => {
         return totalVolume + currentSet.weight * currentSet.reps;
     }, 0);
+}
+
+// Find top set function
+export function getTopSet(sets) {
+
+    if (sets.length === 0) {
+        return null;
+    }
+
+    return sets.reduce((bestSet, currentSet) => {
+        if (currentSet.weight > bestSet.weight) {
+            return currentSet;
+        } else if (currentSet.weight === bestSet.weight && currentSet.reps > bestSet.reps) {
+            return currentSet;
+        } else {
+            return bestSet;
+        }
+    });
 }
