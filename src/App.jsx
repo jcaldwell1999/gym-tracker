b5f510dd13a71a1614ffb950820d2c17c1e520f2
James Caldwell <jamespcaldwell@hotmail.co.uk>
2026-07-08 20:56:19 +0100

    WorkoutItem imported into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import WorkoutItem from './components/WorkoutItem.jsx'
 
 function App() {
   const [workoutName, setWorkoutName] = useState('');
